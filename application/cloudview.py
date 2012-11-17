1cdd015e7eb3a7ae25a43ab687b6ce51ce7e6dd3
Yang Zhenxi <yzx2003209@67-194-206-179.wireless.umnet.umich.edu>
2012-11-17 14:55:28 -0500

    add getmetadata

--- application/cloudview.py
+++ application/cloudview.py
@@ -5,24 +5,10 @@ import gdr_yyt
 import os.path
 
 class cloudview:
-    client_gd = gdr_yyt.GOOGLE_VIEW()
-    client_box = boxdotnet.BoxDotNet()
-    metadata = ''
-    def __init__(self):
-        try:
-            f = open('./metadata.xml', 'rb')
-            self.metadata = boxdotnet.XMLNode.parseXML(f.read())
-            print "local view is at stamp: "+str(self.metadata.view[0]['ts'])
-        except IOError:
-            print 'no file named metadata.xml'
-
-    def sync(self):
-        """not implemented yet"""
-        #download newest metadata
-        client_gd.retrieve_metadata()
+    #client_gd.retrieve_metadata()
         #compare  metas
-        if self.metadata.view[0] == 0:
-            self.metadata = client_gd.metadata
+        #if self.metadata.view[0] == 0:
+        #    self.metadata = client_gd.metadata
         #while loop starts
         """create folder"""
         """compare metadata lists, choose to upload or download according to the timestamp"""
@@ -32,8 +18,8 @@ class cloudview:
         """all the client do authentication"""
         if not self.client_box.authenticated:
             self.client_box.authenticate()
-        if not self.client_gd.Drive_Service:
-            self.client_gd.authent()
+        #if not self.client_gd.Drive_Service:
+        #    self.client_gd.authent()
 
         if self.metadata == '':
             #init a empty metadata file
@@ -57,13 +43,18 @@ class cloudview:
             f = open('metadata.xml', 'wb')
             f.write(self.metadata.convertXML())
             f.close()
-            
 
+    def featureTest(self):
+        metaNode = self.client_box.getmetadata()    
+        if not metaNode == None:
+            print metaNode.convertXML()
+        #self.client_box.upload("metadata.xml", "metadata.xml")
 
 def main():
     print 'app starts'
     cv = cloudview() 
     cv.init()
+    #cv.featureTest()
 
 if __name__ == "__main__":
     main()
