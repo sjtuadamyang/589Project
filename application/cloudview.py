d13320480489ebc3921242bc2fe2a2a44e7dc940
Yang Zhenxi <yzx2003209@67-194-206-179.wireless.umnet.umich.edu>
2012-11-17 17:39:08 -0500

    xml modified

--- application/cloudview.py
+++ application/cloudview.py
@@ -9,6 +9,8 @@ class cloudview:
     client_box = boxdotnet.BoxDotNet()
     client_gd = gdr_yyt.GOOGLE_VIEW()
     metadata = ''
+    cv_loation = ''
+    cv_current_dir = ''
     initialized = False
 
     def __init__(self):
@@ -18,6 +20,8 @@ class cloudview:
             print "local view is at stamp: " + str(self.metadata.view[0]['ts'])
         except IOError:
             print 'no file named metadata.xml'
+        self.cv_location = os.path.dirname(os.path.realpath(__file__))
+        self.cv_current_dir = '/'
 
     def sync(self):
         """create folder
@@ -25,11 +29,12 @@ class cloudview:
 
     def ls(self):
         """not implemented yet"""
-        list = []
+        list = '' 
         if not self.initialized:
             raise Exception(InitError, "application not initialized")
-        for file in metadata.view[0].file
-            list.append(file['fullpath']) 
+        for file in metadata.view[0].file:
+            if file['fullpath'] == cv_current_dir + file['title']:
+                list += '\n'+file[title]
         return list
 
     def add(self, filename):
@@ -39,6 +44,9 @@ class cloudview:
 
     def delete(self):
         """not implemented yet"""
+
+    def cd(self, dir):
+                 
 
     def init(self):
         """all the client do authentication"""
@@ -51,7 +59,7 @@ class cloudview:
             #init a empty metadata file
             """
             <metadata>
-                <view ts='0'/>
+                <view ts='0' cur_id='0'/>
             </metadata>
             """
             self.metadata = boxdotnet.XMLNode()
@@ -59,6 +67,7 @@ class cloudview:
             child = boxdotnet.XMLNode()
             child.elementName = 'view'
             child['ts']='0'
+            child['cur_id']='0'
             try:
                 list = getattr(self.metadata, 'view')
             except AttributeError:
