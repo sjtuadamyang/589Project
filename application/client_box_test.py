b8d404c23fe28ae531d719c60a9115988b766585
Yang Zhenxi <yzx2003209@67-194-200-174.wireless.umnet.umich.edu>
2012-11-21 20:56:35 -0500

    made some change

--- application/client_box_test.py
+++ application/client_box_test.py
@@ -3,12 +3,6 @@ import boxdotnet
 
 client = boxdotnet.BoxDotNet()
 
-file_id = client.upload('test.txt', '1')
-
-client.replace(file_id, 'test.txt')
-
-#client.delete(file_id)
-
-client.download(file_id, 'tt.txt')
+client.deleteall()
 
 
